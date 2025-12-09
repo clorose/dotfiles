384a8134b366f8179fdbdbe96ba4f73ebd3df2dc
clorose <gohan9511@gmail.com>
2025-12-10 02:01:58 +0900

    ✨ Feat: add commit zsh file

--- zsh/.aliases/etc.zsh
+++ zsh/.aliases/etc.zsh
@@ -23,6 +23,30 @@ alias mob="mise outdated --bump"
 # Brewfile 재생성
 alias brewdump='brew bundle dump --describe --force --file="$HOME/dotfiles/Brewfile"'
 
+# --------------------------------------------
+# 📦 고압축 7z 아카이브 생성 (folder → folder.7z)
+# --------------------------------------------
+# @desc: 지정한 폴더를 .7z 고압축 파일로 생성합니다 (7z -mx=9)
+# @usage: 7zz <folder>
+7zz() {
+    local name="${1%/}"
+    echo "📦 Creating $name.7z ..."
+    7z a -mx=9 "$name.7z" "$name/" > /dev/null
+    echo "✨ Done: $name.7z"
+}
+
+# --------------------------------------------
+# 📦 초고압축 tar.xz 생성 (folder → folder.tar.xz)
+# --------------------------------------------
+# @desc: 지정한 폴더를 .tar.xz로 초고압축합니다 (tar -cJf)
+# @usage: txz <folder>
+txz() {
+    local name="${1%/}"
+    echo "📦 Creating $name.tar.xz ..."
+    tar -cJf "$name.tar.xz" "$name/"
+    echo "✨ Done: $name.tar.xz"
+}
+
 # -------------------------
 # 🎸 Guitar (기타)
 # -------------------------
